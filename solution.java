aed34d1867d4cf865b774d3bcad0d2773a80e19c
yagiliao <105021039@live.asia.edu.tw>
2016-09-18 13:31:51 +0800

    Add files via upload

---
+++ solution.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5 , b = 6 , c = 7;
+	if(a+b>c && a+c>b && b+c>a){
+		System.out.println("¬O");
+	}else{
+		System.out.println("§_");
+	}
+	}
+
+}
